2d0f6515fa07b7e0ce0bb099aafa01f0c63e9733
belhard-user <ldarik2l@gmail.com>
2016-09-03 19:46:17 +0400

    query builder

---
+++ app/Http/Controllers/DbController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Barryvdh\Debugbar\Middleware\Debugbar;
+use DB;
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class DbController extends Controller
+{
+    public function select()
+    {
+        $tests = DB::table('tests')->where(function($query){
+            $query->where('email', 't')->where('age', 100);
+        })->get();
+        \Debugbar::info($tests);
+
+        return view('db.db');
+    }
+
+    public function delete()
+    {
+        DB::table('tests')->where('id', 9)->delete();
+        // DB::table('tests')->delete();
+
+        return view('db.db');
+    }
+    public function update()
+    {
+        /*DB::table('tests')
+            ->where('id', 1)
+            ->update([
+            'about' => 'update',
+            'updated_at' => new \DateTime()
+        ]);*/
+        DB::table('tests')
+            ->where('id', 1)
+            ->decriment('age', 5, ['about' => 'updated again']);
+        return view('db.db');
+    }
+    
+    public function insert()
+    {
+        $peoples = [
+            ['email' => 'neo@n.com', 'age' => 21, 'about' => 'the one', 'created_at' => new \DateTime(), 'updated_at' => new \DateTime(), 'gender' => 'M'],
+            ['email' => 'tank@n.com', 'age' => 23, 'about' => 'hacker', 'created_at' => new \DateTime(), 'updated_at' => new \DateTime(), 'gender' => 'M'],
+            ['email' => 'morpheus@n.com', 'age' => 24, 'about' => 'boss', 'created_at' => new \DateTime(), 'updated_at' => new \DateTime(), 'gender' => 'M'],
+            ['email' => 'trinity@n.com', 'age' => 64, 'about' => 'wife', 'created_at' => new \DateTime(), 'updated_at' => new \DateTime(), 'gender' => 'F'],
+        ];
+
+        DB::table('tests')->insert($peoples);
+        /*DB::table('tests')->insert([
+            'email' => 'a@a.com',
+            'age' => 23,
+            'about' => 'about',
+            'created_at' => new \DateTime(),
+            'updated_at' => new \DateTime(),
+            'gender' => 'M'
+        ]);*/
+        return view('db.db');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,11 @@
 <?php
 
 Route::get('/', ['as' => 'home', 'uses' => 'HomeController@home']);
+
+
+Route::group(['prefix' => 'db'], function($route){
+   $route->get('insert', 'DbController@insert');
+   $route->get('update', 'DbController@update');
+   $route->get('delete', 'DbController@delete');
+   $route->get('select', 'DbController@select');
+});
